71f2e0c77757c9f32dadfb5dd12e7a59c78f65e6
De'Yonte W <dw19412@gmail.com>
2013-04-29 13:51:55 -0400

    working plugin

---
+++ gravity-validation.php
@@ -0,0 +1,193 @@
+<?php
+/*
+Plugin Name: Gravity Validation
+Plugin URI: http://example.com/
+Description: Description
+Version: 0.1
+Author: Your Name
+Author URI: http://example.com/
+*/
+
+/**
+ * Copyright (c) 2013 Your Name. All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
+
+//add_action('wp_head','gv_load_script');
+add_action("gform_enqueue_scripts", "gravity_validation", 10, 2);
+add_action('wp_ajax_gravity_validation_ajax','gravity_validation_ajax');
+add_action('wp_ajax_nopriv_gravity_validation_ajax','gravity_validation_ajax');
+
+function gravity_validation($form,$is_ajax){
+	if( !empty($form['gf_inline_validation']) ):
+		echo var_dump($form);
+		wp_enqueue_script( 'jquery');
+		$nonce = wp_create_nonce( 'gv' );
+		$form_id = "gform_".$form['id'];
+		add_action('wp_footer','gv_enqueue_scripts');
+		?>
+		<script type="text/javascript">
+		//http://www.htmlgoodies.com/beyond/javascript/article.php/3724571/Using-Multiple-JavaScript-Onload-Functions.htm
+		function gv_addLoadEvent(func) {
+		  var oldonload = window.onload;
+		  if (typeof window.onload != 'function') {
+		    window.onload = func;
+		  } else {
+		    window.onload = function() {
+		      if (oldonload) {
+		        oldonload();
+		      }
+		      func();
+		    }
+		  }
+		}
+
+		function gv_<?php echo $form['id'];?>(){
+			jQuery('form#gform_<?php echo $form["id"];?>').bind('jqv.field.result', function(event, field, errorFound, prompText){
+				var field_container = jQuery(field).parents('li:first');
+				if( errorFound ){
+					field_container.addClass('gravity_validation gverror');
+
+				}
+				else if(!errorFound){
+					field_container.addClass('gravity_validation gvsuccess');
+				}
+			});
+			<?php
+			//boolean to determine if the form has any required fields
+			$formvalidate = false;
+			foreach( $form["fields"] as $field ):
+				if( $field['isRequired'] ):
+					//validate required field
+					$formvalidate = true;
+				endif;
+			endforeach;
+			if($formvalidate):?>
+				jQuery('form#gform_<?php echo $form["id"];?>').validationEngine();
+				jQuery(window).ready(function(){
+					var field_type = null;
+					jQuery('form#gform_<?php echo $form["id"];?> li.gfield_contains_required input, form#gform_<?php echo $form["id"];?> li.gfield_contains_required select, form#gform_<?php echo $form["id"];?> li.gfield_contains_required textarea').each(function(){
+						var field_id = '#'+jQuery(this).attr('id');
+						if( jQuery(field_id).attr('type') == 'radio' ){
+							jQuery(field_id).attr('data-validation-engine','validate[required]');
+						}
+						else{
+							jQuery(field_id).attr('data-validation-engine','validate[required]');
+						}
+						jQuery(field_id).attr('data-errormessage','<?php echo (isset($field["errorMessage"])?$field["errorMessage"]:"This field is required");?>');
+						jQuery(field_id).attr('data-prompt-position','topRight:5');
+						console.log(field_id);
+					});
+				});	
+			<?php endif;?>
+
+			jQuery('form#<?php echo $form_id;?> input[type="text"], form#<?php echo $form_id;?> textarea, form#<?php echo $form_id;?> select').on('focus',function(){
+				var field_container = jQuery(this).parents('li.gfield');
+				field_container.removeClass('gravity_validation gverror');
+				field_container.removeClass('gravity_validation gvsuccess');
+				field_container.children('p.gverror').remove();
+			});
+		}
+		gv_addLoadEvent(gv_<?php echo $form['id'];?>);
+		</script>
+		
+	<?php
+	endif;
+}
+
+//add_action("gform_field_css_class", "gravity_validation_class", 10, 3);
+
+function gravity_validation_class($classes, $field, $form){
+	if( $field['isRequired'] )
+		$classes .= " validate[required]";
+
+	return $classes;
+}
+
+function gravity_validation_ajax(){
+	check_ajax_referer( 'gv' );
+	$required = $_POST['field_required'];
+	if( $required == "true" ):
+		$value = $_POST['field_value'];
+
+		if( empty($value) ):
+			$result = array('type'=>'error');
+		elseif( !empty($value) ):
+			$result = array('type'=>'success');
+		endif;
+
+	elseif( $required == "false" ):
+		$result = array('type'=>'success');
+	endif;
+
+	header('Content-Type: application/json');
+	header("Cache-Control: no-cache, must-revalidate");
+	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+	echo json_encode($result);
+	die();
+}
+
+
+function gv_enqueue_scripts(){
+	wp_register_style('gravity.validation', plugins_url('css/gravity.validation.css',__FILE__), false, '0.1');
+	wp_enqueue_style('gravity.validation');
+	wp_register_style('jquery.validation.engine', plugins_url('js/jquery.validation.engine/css/validationEngine.jquery.css',__FILE__), false, '2.6.1');
+	wp_enqueue_style('jquery.validation.engine');
+	wp_register_script('jquery.validation.engine', plugins_url('js/jquery.validation.engine/js/jquery.validationEngine.js',__FILE__), array('jquery'), '2.6.1', true );
+	wp_enqueue_script('jquery.validation.engine');
+	wp_register_script('jquery.validation.engine.en', plugins_url('js/jquery.validation.engine/js/languages/jquery.validationEngine-en.js',__FILE__), array('jquery'), '2.6.1', true );
+	wp_enqueue_script('jquery.validation.engine.en');
+}
+
+
+function gv_load_script(){
+	
+	?>
+
+<?php
+}
+
+
+
+add_filter('gform_form_settings', 'gf_inline_validation', 10, 2);
+function gf_inline_validation($settings, $form) {
+
+	$gf_inline_validation = rgars($form, 'gf_inline_validation');
+	if( !empty($gf_inline_validation) )
+		$gf_inline_validate = 'checked="checked"';
+	else
+		$gf_inline_validate = null;
+	
+    $settings['Form Options']['gf_inline_validation'] = '
+        <tr>
+            <th>Inline Validation <a href="javascript:void(0);" class="tooltip tooltip_form_field_placeholder" tooltip="&lt;h6&gt;Enable Inline Validation &lt;/h6&gt;Validate gravity forms without user submitting form">(?)</a></th>
+            <td><input value="1" '.$gf_inline_validate.' id="gf_inline_validation" name="gf_inline_validation" type="checkbox"> <label for="gf_inline_validation">Enable inline validation</label></td>
+        </tr>';
+
+    return $settings;
+}
+
+// save your custom form setting
+add_filter('gform_pre_form_settings_save', 'save_my_custom_form_setting');
+function save_my_custom_form_setting($form) {
+
+    $form['gf_inline_validation'] = rgpost('gf_inline_validation');
+    return $form;
+}
